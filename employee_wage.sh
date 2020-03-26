281e76583a093114b02021dc7cd4b9ba27c14f62
murali1125 <murali1125s@gmail.com>
2020-03-26 23:18:03 +0530

    uc1: randomly selecty the employee present or absent

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,7 +1,10 @@
 #! /bin/bash -x
 
-
+# Start with Displaying welcome to Employee Wage Computation Program on Master Branch
 
 #--------------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
+
+# randomly select attedance of the employee 2-parttime present 1-ful time present 0-absent
+attendance=$(( RANDOM % 3 ))
