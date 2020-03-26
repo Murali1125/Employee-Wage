7a29b447281d29dc5b8f19425b447cb8539258e8
murali1125 <murali1125s@gmail.com>
2020-03-26 23:56:58 +0530

    uc6: Condition max hours=100 days=20 added

--- employee_wage.sh
+++ employee_wage.sh
@@ -10,22 +10,45 @@
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
 
 
-# wage variable to store total wage
+# variables
 total_wage=0
 wage=20
 working_hrs=8
 part_wrk_hrs=4
+hours=0
+days=0
 
-# for month(20days) repete it for 20 times
-for (( i=1;i<=20;i++ ))
-do
+function work_hours () {
+	if  [[ $days -gt 20 -o $hours -ge 100 ]]
+	then
+		return
+	fi
 	# randomly select attedance of the employee 2-parttime present 1-ful time present 0-absent
 	attendance=$(( RANDOM % 3 ))
+	# if employee present calculate wage
+	case $attedance in 
+		1) #increase the days and hourse
+			hours=$(( hours + 8 ))
+			if [[ $hours -gt 100 ]]
+			then
+				hours=100
 
-	# caluculate total wage as per attandace by using case statement
-	case $attendance  in 
-		1)	total_wage=$(( total_wage + ( wage * working_hrs ) )) ;;
+			fi
+			;;
+		2)  #if it part increase hours + 4
+			hours=$(( hours + 4 )) 
+			if [[ $hours -gt 100 ]]
+                        then
+                                hours=100
 
-		2)	 total_wage=$(( total_wage + ( wage * part_wrk_hrs ) )) ;;
+                        fi
+			;;
+		0) # absent then increase the day only no hours
 	esac
-done
+	(( days++ ))
+	work_hours
+}
+
+# total wage = total hours * wage
+total_wage=$(( hours * wage ))
+echo " wage of the employee  =  $total_wage "
