fe7f51fdbcc9a1f6a3b7a938bc356e17c4f916e1
murali1125 <murali1125s@gmail.com>
2020-03-26 23:31:46 +0530

    uc3 : part time employee wage calculation

--- employee_wage.sh
+++ employee_wage.sh
@@ -2,6 +2,7 @@
 
 # Start with Displaying welcome to Employee Wage Computation Program on Master Branch
 # Calculate Daily Employee Wage assume per hour wage =20 daily working hours 8
+#Add Part time Employee & Wage - Assume Part time Hour is 4
 #--------------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
@@ -14,4 +15,14 @@ attendance=$(( RANDOM % 3 ))
 total_wage=0
 wage=20
 working_hrs=8
-total_wage=$(( wage * working_hrs ))
+part_wrk_hrs=4
+
+# caluculate total wage as per attandace
+if [[ $attendance -eq 1 ]]
+then
+	total_wage=$(( wage * working_hrs ))
+elif [[ $attendance -eq 2 ]]
+then
+	 total_wage=$(( wage * part_wrk_hrs ))
+
+fi
